451b34a9ef333904c022e35614c0680638f93195
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-08-28 17:03:24 +0100

    set-up posgres on render.com, connect to DB from Node.js

---
+++ src/models/Product.ts
@@ -0,0 +1,80 @@
+import {
+  AllowNull,
+  Model,
+  Column,
+  Table,
+  DataType,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'products',
+  createdAt: false,
+  updatedAt: false,
+})
+export class Product extends Model {
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    category: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    itemId: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    name: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+  })
+    fullPrice: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+  })
+    price: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    screen: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    capacity: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    color: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    ram: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+  })
+    year: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+    image: string;
+};
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { sequelize } from './utils/db';
 
 dotenv.config();
 
 const { CLIENT_URL, PORT } = process.env;
 
 const app = express();
+const start = sequelize.authenticate();
+
+console.log(start);
 
 app
   .use(cors({ origin: CLIENT_URL }))
